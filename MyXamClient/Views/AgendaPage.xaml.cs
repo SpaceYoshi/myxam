4ebaa3a86779920089875cf9722141b17567f4d4
Sam-D-C <114139132+Sam-D-C@users.noreply.github.com>
2024-01-22 14:23:02 +0100

    ContentPage error

---
+++ MyXamClient/Views/AgendaPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyXamClient.Views;
+
+public partial class AgendaPage : ContentPage
+{
+	public AgendaPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyXamClient/Views/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyXamClient.Views;
+
+public partial class HomePage : ContentPage
+{
+    int count = 0;
+
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
