5408af415b3d72daf5771da93d49472708f8e039
Sam-D-C <114139132+Sam-D-C@users.noreply.github.com>
2024-01-22 13:36:49 +0100

    First hand TcpConnection

---
+++ MyXamClient/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyXamClient.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EventType Type { get; set; }
+        public string Description { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+    public enum EventType
+    {
+        School, Personal, Work, Sports
+    }
+}
---
+++ MyXamClient/Services/TcpConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyXamClient.Services
+{
+    public class TcpConnection
+    {
+        public async static void WriteMessage(TcpClient client, byte[] message)
+        {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            stream.Write(message);
+            stream.Flush();
+        }
+
+        public static string ReadTextMessage(TcpClient client)
+        {
+            StreamReader stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            return stream.ReadLine();
+        }
+    }
+}
