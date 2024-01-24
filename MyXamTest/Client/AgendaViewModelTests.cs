bb674fe7c691549e8e9fe7e497786912ba52d83b
Sam-D-C <114139132+Sam-D-C@users.noreply.github.com>
2024-01-24 23:03:40 +0100

    Tests classes

---
+++ MyXamTest/Client/AgendaViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyXamClient.Services;
+using MyXamLibrary.Models;
+using MyXamClient.ViewModels;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace MyXamTest.Client
+{
+    internal class AgendaViewModelTests
+    {
+    }
+}
